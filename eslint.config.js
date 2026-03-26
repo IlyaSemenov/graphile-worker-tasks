1c0a33a196d21280a1bde380a710742001996510
Ilya Semenov <ilya@semenov.co>
2026-03-26 17:03:19 +0700

    Update linting setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,4 @@
 
 import { defineConfig } from "@ilyasemenov/eslint-config"
 
-export default defineConfig().append({
-  files: ["**/*.md/*.{js,ts}"],
-  rules: {
-    "no-console": "off",
-  },
-})
+export default defineConfig()
